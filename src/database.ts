931acbfbfe3547db654ee59003935b1b570f3826
Luis Pérez <luis.perez@geekdacity.com>
2022-05-23 23:03:23 -0600

    feat: Add database file

---
+++ src/database.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB_DEV,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PWD,
+  ENV
+} = process.env;
+
+// Always use dev env as default
+let client: Pool = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB_DEV,
+  user: POSTGRES_USER,
+  password: POSTGRES_PWD
+});
+
+if (ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PWD
+  });
+}
+
+export default client;
+
+

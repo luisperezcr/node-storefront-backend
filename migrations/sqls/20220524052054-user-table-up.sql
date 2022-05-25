91ef9a398e084ec1fe28e6c5860117fa3bbc9e0a
Luis Pérez <luis.perez@geekdacity.com>
2022-05-24 21:49:05 -0600

    refactor: Adding new field to users migrations

--- migrations/sqls/20220524052054-user-table-up.sql
+++ migrations/sqls/20220524052054-user-table-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, firstName VARCHAR(50), lastName VARCHAR(50), password TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50), firstName VARCHAR(50), lastName VARCHAR(50), password TEXT);

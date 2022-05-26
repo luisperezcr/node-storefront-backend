775fd51c82ed3583f67ed2237aa32934a1d24542
Luis Pérez <luis.perez@geekdacity.com>
2022-05-26 15:24:00 -0600

    test: Implement Categories model tests

---
+++ src/tests/categories.spec.ts
@@ -0,0 +1,35 @@
+import { Category, CategoryStore } from '../models/categories';
+
+const store = new CategoryStore();
+const category: Category = {
+  name: 'Food'
+};
+
+describe('Categories Model', () => {
+  it('should have index method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have show method', () => {
+    expect(store.show).toBeDefined();
+  });
+
+  it('should have create method', () => {
+    expect(store.create).toBeDefined();
+  });
+  
+  it('should get categories', async() => {
+    const result = await store.index();
+    expect(result).toEqual([]);
+  });
+
+  it('should create a category', async() => {
+    const result = await store.create(category);
+    expect(result.name).toEqual(category.name);
+  });
+
+  it('should get a category by ID', async() => {
+    const result = await store.show(1);
+    expect(result?.name).toEqual(category.name);
+  });
+});
